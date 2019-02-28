199b3a62caf73e2c962030bb89ce115c5c095ef4
raspberry <d@d.fr>
2019-02-28 20:36:37 +0100

    ajout host 0.0.0.0 dan setting.py

--- pese_canards/settings.py
+++ pese_canards/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f7hcln(i#&a*u^(dwi7xib5!-wt^5ze(*r62v#@@@)w3dtr+*7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [0.0.0.0]
 
 
 # Application definition
